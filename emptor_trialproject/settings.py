3cda3fa6ce430208aca73a7dba0e8f1e459c78cb
NCJo <cnimkulr@gmail.com>
2020-01-28 02:30:30 -0700

    Fix state management for showing endpoint from Django server

--- emptor_trialproject/settings.py
+++ emptor_trialproject/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'world_development_indicators_backend.apps.WorldDevelopmentIndicatorsBackendConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +52,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://localhost:8000',
 ]
 
 ROOT_URLCONF = 'emptor_trialproject.urls'
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: []
+  };
+/* 
+   This is where the magic happens
+*/
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://127.0.0.1:8000/display_data/'); // fetching the data from api, before the page loaded
+      const todos = await res.json();
+      this.setState({
+        todos
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.todos.map(item => (
+          <div key={item.id}>
+            <h1>{item.country_name}</h1>
+            <span>{item.country_code}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
